fe15b8c61f7f219b178016042d2d6c7e9a8aeaf2
HugoLandin <80512066+HugoLandin@users.noreply.github.com>
2021-03-14 00:35:39 +0100

    chore(scripts): implemented camera following script
    Created "SecurityCamera" C# script which follows a target, currently set to "CameraTargetTest" object in Scene.

---
+++ Assets/Scripts/SecurityCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SecurityCamera : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+
+    private void Update()
+    {
+        transform.LookAt(targetTransform);
+    }
+}
