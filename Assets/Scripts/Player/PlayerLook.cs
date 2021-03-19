ebfd64d623855143b148403c338e1cc8c1a700da
oles <olchyk98@gmail.com>
2021-03-19 11:04:42 +0100

    chore(scripts): player look (#4)
    * feat(scripts): player look script
    
    * chore(scripts): change namespace for input handler

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerLook : MonoBehaviour
+    {
+        [Range(1f, 200f)] [SerializeField] private float _mouseSensitivity;
+        [SerializeField] private Transform _cameraTransform;
+        private Transform _transform;
+        private InputActionHandler _input;
+
+        private float _rotationY = 0f;
+
+        private void Start()
+        {
+            _transform = GetComponent<Transform>();
+            _input = GetComponent<InputActionHandler>();
+        }
+
+        private void Update()
+        {
+            HandleLookTick();
+        }
+
+        private void HandleLookTick ()
+        {
+            var mouseDirection = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+            var horizontalRotation = _transform.rotation.eulerAngles;
+            var nextY = _rotationY - mouseDirection.y * _mouseSensitivity;
+
+            _rotationY = Mathf.Clamp(nextY, -90f, 90f);
+            horizontalRotation.y += mouseDirection.x * _mouseSensitivity;
+
+            // Apply rotation changes
+            _cameraTransform.localRotation = Quaternion.Euler(_rotationY, 0f, 0f);
+            _transform.rotation = Quaternion.Euler(horizontalRotation);
+        }
+    }
+}
