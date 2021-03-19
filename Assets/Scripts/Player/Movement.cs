451c59afef9c9ea0b273ee53627f9e281dab75b9
k0ded <59554035+k0ded@users.noreply.github.com>
2021-03-19 10:49:15 +0100

    chore: Place movement related parameters in the movement script

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Player.Input;
+using UnityEngine;
+
+[RequireComponent(typeof(InputActionHandler))]
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float povSpeed;
+    [SerializeField] private float jumpHeight;
+    
+    private void Start()
+    {
+        var input = GetComponent<InputActionHandler>();
+        input.OnMove += OnMove;
+        input.OnRotate += OnRotate;
+        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnRotate(Vector3 rotation)
+    {
+        
+    }
+
+    private void OnMove(Vector3 input)
+    {
+        
+    }
+}
