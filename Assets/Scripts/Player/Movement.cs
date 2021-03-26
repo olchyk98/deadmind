a206b231811661e01ada840cdef37b9612e48be4
k0ded <k0dedgem@gmail.com>
2021-03-26 10:33:48 +0100

    feat: Movement epicness (#6)
    * feat: Movement epicness
    
    * fuck you
    
    Co-authored-by: Tristan <tristan@tristanfarkas.dev>

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using Player;
+using System;
 using Player;
 using UnityEngine;
 
 [RequireComponent(typeof(InputActionHandler))]
+[RequireComponent(typeof(Collider))]
 [RequireComponent(typeof(Rigidbody))]
 public class Movement : MonoBehaviour
 {
     private Rigidbody rb;
+    private bool grounded;
 
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float povSpeed;
-    [SerializeField] private float jumpHeight;
+    [SerializeField] [Range(1f, 20f)] private float movementSpeed;
+    [SerializeField] [Range(1f, 20f)] private float jumpHeight;
+    [SerializeField] private string groundTag;
 
     private void Start()
     {
@@ -24,6 +25,37 @@ public class Movement : MonoBehaviour
 
     private void OnMove(Vector3 input)
     {
+        var jumping = input.y;
+        var movement = new Vector3(input.x*movementSpeed*10, rb.velocity.y, input.z*movementSpeed*10);
+        
+        rb.velocity = movement;
+        if (jumping > 0 && grounded)
+        {
+            Jump(jumping);
+        }
 
+    }
+
+    private void Jump(float input)
+    {
+        rb.AddForce(Vector3.up * (jumpHeight * input), ForceMode.Impulse);
+    }
+
+   
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag(groundTag))
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag(groundTag))
+        {
+            grounded = false;
+        }
     }
 }
