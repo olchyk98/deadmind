3198f2ae182573c8e3431eabc9581e01077d9cd2
HugoLandin <80512066+HugoLandin@users.noreply.github.com>
2021-04-03 15:03:40 +0200

    Feat: BackgroundWordSpawner
    Created a script for Main Menu visuals that spawns random words from a list on random y-positions in the background and makes them float across the screen.

---
+++ Assets/Scripts/BackgroundWordSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BackgroundWordSpawner : MonoBehaviour
+{
+    public List<string> WordList;
+    public GameObject textPrefab;
+    void Update()
+    {
+        if (!IsInvoking("SpawnWord"))
+        {
+            Invoke("SpawnWord", 0.3f);
+        }
+    }
+    void SpawnWord()
+    {
+        GameObject spawnedText = Instantiate(textPrefab, transform);
+        spawnedText.GetComponent<Rigidbody>().velocity = transform.right * Random.Range(100,300);
+        spawnedText.GetComponent<TextMeshProUGUI>().text = WordList[Random.Range(0, WordList.Count)];
+        spawnedText.GetComponent<Transform>().transform.localPosition = new Vector3(spawnedText.transform.localPosition.x, Random.Range(-200,200), spawnedText.transform.localPosition.z);
+        Destroy(spawnedText, 15);
+    }
+}
