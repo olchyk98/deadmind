ff943eb91f52742f4cc03fe86d339b44fe46686b
HugoLandin <hugo@fixstar.net>
2021-04-23 23:11:03 +0200

    Feat (Keypad.cs) started work on keypad
    Started work on Keypad script and Object, about 80% done.

---
+++ Assets/Button3D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button3D : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent OnPress;
+    Animation animation;
+    private void Start()
+    {
+        animation = GetComponent<Animation>();
+    }
+    private void OnMouseDown()
+    {
+        OnPress.Invoke();
+        if(animation != null)animation.Play();
+    }
+}
