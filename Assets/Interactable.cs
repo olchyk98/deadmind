29535dd576e449e143bc83bdb497bb62e137a986
HugoLandin <hugo@fixstar.net>
2021-04-24 18:13:35 +0200

    Feat(Interactables & Doors)
    Created Interactable Script & Door Script

---
+++ Assets/Interactable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class Interactable : MonoBehaviour
+{
+    public string interactText;
+    Collider objectCollider;
+    [SerializeField]
+    UnityEvent interactEvent;
+    private void Start()
+    {
+        objectCollider = GetComponent<Collider>();
+    }
+    public void Hit()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            interactEvent.Invoke();
+        }
+    }
+}
