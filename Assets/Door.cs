6422ad0d0db0c81d857702bd302f63307f36e111
HugoLandin <80512066+HugoLandin@users.noreply.github.com>
2021-04-24 18:21:52 +0200

    feat: implement keypad and door mechanics (#23)

---
+++ Assets/Door.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void ToggleState()
+    {
+            animator.SetBool("isOpen", !animator.GetBool("isOpen"));
+    }
+}
---
+++ Assets/PlayerSideInteractable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class PlayerSideInteractable : MonoBehaviour
+{
+    [SerializeField]
+    Transform cameraTransform;
+    [SerializeField]
+    TextMeshProUGUI interactText;
+    RaycastHit? raycastHit;
+    void Update()
+    {
+        Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+        Physics.Raycast(ray, out var hit, 5);
+        raycastHit = hit;
+        Interactable interactable = raycastHit?.collider?.gameObject?.GetComponent<Interactable>();
+        if (interactable != null)
+        {
+            interactText.text = interactable.interactText;
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                interactable.Hit();
+            }
+        }
+        else
+        {
+            interactText.text = "";
+        }
+    }
+}
