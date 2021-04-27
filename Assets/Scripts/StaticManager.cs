271439093a985336a91b7335749b14f2f55ea233
HugoLandin <hugo@fixstar.net>
2021-04-27 15:27:15 +0200

    chore(Main Menu) Improved Main Menu script
    Improved the Main Menu with new LevelLoading

--- Assets/Scripts/StaticManager.cs
+++ Assets/Scripts/StaticManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class StaticManager : MonoBehaviour
 {
     public static List<int> levelIndexList;
+    public static int curLevelIndex;
     private void Awake()
     {
         DontDestroyOnLoad(this);
