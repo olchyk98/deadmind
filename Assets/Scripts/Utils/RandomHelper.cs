13f7a2145b6faf13960b3ae871b6b7780d8338d1
k0ded <59554035+k0ded@users.noreply.github.com>
2021-05-11 10:37:17 +0200

    prrrt prrrt lol xd LEZZ GOOOOO

---
+++ Assets/Scripts/Utils/RandomHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace Utils
+{
+    public static class RandomHelper
+    {
+        
+        /// <summary>
+        /// Gives you a seemingly random value that doesn't change if the input and clamping stays the same
+        /// </summary>
+        public static int getValue(string input, int min, int max)
+        {
+            var i = input.Aggregate(1, (current, c) => current * c);
+            i %= max - min;
+            i += min;
+            return i;
+        }
+    }
+}
