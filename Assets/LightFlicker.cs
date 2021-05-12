f11856fcfc0c23577e61994b48c902846ca98cf4
Oles Odynets <olchyk98@gmail.com>
2021-05-12 15:07:27 +0200

    feat(core): implement painting system

---
+++ Assets/LightFlicker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    private Light[] _lights;
+    [SerializeField]
+    private int _curNumber = 5;
+    void Start()
+    {
+        _lights = GetComponentsInChildren<Light>();
+    }
+    void Update()
+    {
+        _curNumber += Random.Range(-1, 2);
+        _curNumber = Mathf.Clamp(_curNumber, 0, 10);
+
+        foreach (Light light in _lights)
+        {
+            light.enabled = (_curNumber >= 5);
+        }
+    }
+}
---
+++ Assets/Painting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painting : Interactable
+{
+    private PaintingManager paintingManager;
+    public Light light;
+    public MeshRenderer paintingRenderer;
+    private void Start()
+    {
+        paintingRenderer = GetComponentInChildren<MeshRenderer>();
+        light = GetComponentInChildren<Light>();
+        paintingManager = GetComponentInParent<PaintingManager>();
+        OnInteract += SetSelected;
+    }
+    private void Update()
+    {
+        light.enabled = object.ReferenceEquals(paintingManager.selectedPainting, this);
+    }
+    private void SetSelected()
+    {
+        paintingManager.selectedPainting = this;
+    }
+}
